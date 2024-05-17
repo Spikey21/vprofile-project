f778affc5d366d48541ed46f6821c7e677afd3e4
Spikey21 <rafal.paciorek19@gmail.com>
2024-05-17 20:52:06 +0200

    add AWS branch

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -13,6 +13,7 @@ cd /tmp/
 git clone -b main https://github.com/hkhcoder/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
@@ -28,6 +29,9 @@ sudo systemctl restart mariadb
 
 
 #starting the firewall and allowing the mariadb to access from port no. 3306
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
 sudo firewall-cmd --get-active-zones
